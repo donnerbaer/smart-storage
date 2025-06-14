5812095cadf496f72578c121edb92f22b3251d9d
donnerbaer <45535297+donnerbaer@users.noreply.github.com>
2025-06-14 05:26:59 +0200

    Update storage.py

--- app/views/storage.py
+++ app/views/storage.py
@@ -2,12 +2,14 @@
 This module provides routes to display storage locations and individual storage details.
 """
 
+import os
 from flask import Blueprint, render_template
-from flask import request
+from flask import request, redirect, url_for
 from flask_babel import gettext as _
 from flask_login import login_required, current_user
 from app import db
-from app.resource.storage_location.model import StorageLocation
+from app.resource.storage_location.model import StorageLocation, StorageLocationImage
+from app.resource.item.model import ItemStorageStock
 from app.resource.storage_location.storage import get_storage_hierarchy
 
 
@@ -45,6 +47,38 @@ def storage_view(storage_id):
                            qrcode_url=qrcode_url,
                            storage_hierarchy=get_storage_hierarchy(storage_id)
                            )
+
+@storage_bp.route('/storages/<int:storage_id>/delete', methods=['GET'])
+@login_required
+def delete_storage(storage_id):
+    """ Delete a storage location.
+    
+    Args:
+        storage_id (int): The ID of the storage location to delete.
+
+    Returns:
+        Redirects to the storages page after deletion.
+    """
+    storage = db.session.query(StorageLocation).filter_by(id=storage_id).first_or_404()
+    
+    # remove all images associated with the storage location
+    # from the filesystem and database
+    images = db.session.query(StorageLocationImage).filter_by(storage_location_id=storage.id).all()
+    for image in images:
+        image_path = os.path.join('img', 'storage', image.filename)
+        if os.path.exists(image_path):
+            os.remove(image_path)
+        db.session.delete(image)
+
+    # remove all item stocks associated with the storage location
+    item_stocks = db.session.query(ItemStorageStock).filter_by(storage_location_id=storage.id).all()
+    for stock in item_stocks:
+        db.session.delete(stock)
+
+    # remove the storage location itself
+    db.session.delete(storage)
+    db.session.commit()
+    return redirect( url_for('storage.storages_view') )
 
 
 @storage_bp.route('/api/storages/list/childs/<int:storage_id>', methods=['GET'])
