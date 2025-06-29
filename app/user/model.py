2119db6bb18e0f27b218f1ecd79ad7fdc0d9da3d
donnerbaer <45535297+donnerbaer@users.noreply.github.com>
2025-06-29 11:50:51 +0200

    Update model.py
    add documentation

--- app/user/model.py
+++ app/user/model.py
@@ -19,13 +19,38 @@ class User(UserMixin, db.Model):
     image_filename = db.Column(db.String(256), nullable=True)  # For user profile image
 
     def set_password(self, password) -> None:
+        """ Set the user's password by hashing it. 
+
+        Args:
+            password (str): The plaintext password to be hashed and stored.
+
+        Returns:
+            None
+        """
         self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password: str) -> str:
+    def check_password(self, password: str) -> bool:
+        """ Check if the provided password matches the stored password hash.
+
+        Args:
+            password (str): The plaintext password to check against the stored hash.
+
+        Returns:
+            bool: True if the password matches, False otherwise.
+        """
         return check_password_hash(self.password_hash, password)
 
     def has_permission(self, permission_name: str) -> bool:
-        """ Check if the user has a specific permission. """
+        """ Check if the user has a specific permission.
+        This method checks if the user belongs to any group that has the specified permission.
+        This is done by iterating through the user's groups and their roles to find the permission.
+
+        Args:
+            permission_name (str): The name of the permission to check.
+
+        Returns:
+            bool: True if the user has the permission, False otherwise.
+        """
         for group in self.groups:
             for role in group.roles:
                 if any(permission.name == permission_name for permission in role.permissions):
