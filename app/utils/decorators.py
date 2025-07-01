4fb9e65c9926b9750711b2d363573572fed37024
donnerbaer <45535297+donnerbaer@users.noreply.github.com>
2025-07-01 22:55:07 +0200

    Update decorators.py

--- app/utils/decorators.py
+++ app/utils/decorators.py
@@ -47,3 +47,33 @@ def check_permissions(required_permissions: List[str]) -> callable:
             return f(*args, **kwargs)
         return decorated_function
     return decorator
+
+
+def check_own_or_has_permissions(required_permissions: List[str]) -> callable:
+    """ Decorator to check if the user is accessing their own resource or has the required permissions.
+    This is useful for routes where users can only access their own data or need specific permissions to access certain resources.
+
+    Args:
+        required_permissions (List[str]): A list of permission names that the user must have.
+
+    kwargs:
+        user_id (int): The ID of the user whose resource is being accessed.
+                If this is provided and matches the current user's ID, access is granted regardless of permissions.
+
+    Returns:
+        callable: A decorator that checks if the user is accessing their own resource or has the required permissions.
+    """
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            user_id = kwargs.get('user_id')
+            if user_id is not None and current_user.id == user_id:
+                return f(*args, **kwargs)
+
+            for required_permission in required_permissions:
+                if not current_user.has_permission(required_permission):
+                    print(f"User {current_user.username} does not have permission: {required_permission}")
+                    abort(403)
+            return f(*args, **kwargs)
+        return decorated_function
+    return decorator
